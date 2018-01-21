d2b5adef068659ddf06d18fe85407cda5f462533
kowsalyavsb <31506020+kowsalyavsb@users.noreply.github.com>
2018-01-21 15:37:20 +0530

    Create multiple.c

---
+++ multiple.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+        int i,n;
+     
+        printf("\n  Give The Value of N:  ");
+        scanf("%d",&n);
+        for(i=1;i<=n;i++)
+        {
+            printf("  %d",n*i);
+        }
+        
+        return 0;
+}
+
+
